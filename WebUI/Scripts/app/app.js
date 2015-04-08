3b277d019e9605a60e53cba26868587c1e01f561
rmethani <rozina.methani@gmail.com>
2015-04-08 01:37:39 -0400

    Initial setup

---
+++ WebUI/Scripts/app/app.js
@@ -0,0 +1,21 @@
+﻿
+var dentalApp = angular.module('dentalApp', [
+  'ngRoute','ui.bootstrap',
+  'dentalControllers'
+]);
+
+dentalApp.config(['$routeProvider',
+  function ($routeProvider) {
+      $routeProvider.
+        when('/phones', {
+            templateUrl: 'partials/phone-list.html',
+            controller: 'PhoneListCtrl'
+        }).
+        when('/phones/:phoneId', {
+            templateUrl: 'partials/phone-detail.html',
+            controller: 'PhoneDetailCtrl'
+        }).
+        otherwise({
+            redirectTo: '/phones'
+        });
+  }]);
---
+++ WebUI/Scripts/app/controllers.js
@@ -0,0 +1,32 @@
+﻿
+var dentalControllers = angular.module('dentalControllers', []);
+
+dentalControllers.controller('PhoneListCtrl', ['$scope', '$http',
+  function ($scope, $http) {
+      $http.get('phones/phones.json').success(function (data) {
+          $scope.phones = data;
+      });
+
+      $scope.orderProp = 'age';
+  }]);
+
+dentalControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
+  function ($scope, $routeParams) {
+      $scope.phoneId = $routeParams.phoneId;
+  }]);
+
+dentalControllers.controller('CarouselDemoCtrl', function ($scope) {
+    $scope.myInterval = 3000;
+    var slides = $scope.slides = [];
+    $scope.addSlide = function () {
+        var newWidth = 600 + slides.length + 1;
+        slides.push({
+            image: 'http://placekitten.com/' + newWidth + '/300',
+            text: ['More', 'Extra', 'Lots of', 'Surplus'][slides.length % 4] + ' ' +
+              ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+        });
+    };
+    for (var i = 0; i < 4; i++) {
+        $scope.addSlide();
+    }
+});
